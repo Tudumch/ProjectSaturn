7db0ed2ae4093fe1c88e68e1121cb65e3e5478ba
Plekhanov Sergei <567-45@mail.ru>
2025-02-05 12:04:30 +0300

    Add initial actor for RechargingCapsule

---
+++ Source/ProjectSaturn/Public/Props/PS_Prop_RechargingCapsule.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Props/PS_Prop_Base.h"
+#include "PS_Prop_RechargingCapsule.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSATURN_API APS_Prop_RechargingCapsule : public APS_Prop_Base
+{
+	GENERATED_BODY()
+	
+};
