537644d11d89a0fc322dac2b360c23b5156b9f6c
Plekhanov Sergei <567-45@mail.ru>
2025-07-31 10:28:10 +0300

    Enable Online Subsystem; Add Main Menu placeholder

--- Source/ProjectSaturn/ProjectSaturn.Build.cs
+++ Source/ProjectSaturn/ProjectSaturn.Build.cs
@@ -8,7 +8,7 @@ public class ProjectSaturn : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
