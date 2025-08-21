22697ceefb55076c447ebe4a6c2766e95838d310
Plekhanov Sergei <567-45@mail.ru>
2025-08-21 09:21:15 +0300

    Connect GAS plugin

--- Source/ProjectSaturn/ProjectSaturn.Build.cs
+++ Source/ProjectSaturn/ProjectSaturn.Build.cs
@@ -8,7 +8,9 @@ public class ProjectSaturn : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam", "OnlineSubsystemUtils"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", 
+            "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam", "OnlineSubsystemUtils", 
+            "GameplayAbilities", "GameplayTags", "GameplayTasks"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/ProjectSaturn/Public/GAS/PS_AttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "PS_AttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSATURN_API UPS_AttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
